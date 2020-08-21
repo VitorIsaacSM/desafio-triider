19c9c4a6b664cf9016fef5e52ba304c8114bc0af
Vitor Maia <vitor2001bidi@gmail.com>
2020-08-21 20:28:06 -0300

    adiciona interceptor com token de autorizacao

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,4 +4,12 @@ const Api = axios.create({
     baseURL: 'http://localhost:3001'
 });
 
+Api.interceptors.request.use(config => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+}, err => Promise.reject(err));
+
 export default Api;
